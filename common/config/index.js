b99bfc32622e23dfca023b496ec7a91268d312d5
陆凤林(lufl) <18701100747@163.com>
2025-07-15 21:11:12 +0800

    feat: change

--- common/config/index.js
+++ common/config/index.js
@@ -6,8 +6,10 @@ const config = {
 	TENANT_ENABLED: false, // 租户开关
 }
 if (process.env.NODE_ENV === 'development') {
+	// config.baseUrl = 'http://192.168.0.72:8080';
 	config.baseUrl = 'http://10.180.7.118:8180';
 } else {
+	// config.baseUrl = 'http://192.168.0.72:8080';
 	config.baseUrl = 'http://10.180.7.118:8180';
 }
 export default config;
